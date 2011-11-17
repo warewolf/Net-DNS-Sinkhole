6f85b7130d5e42ad3b4f4efb42c58e7ca271a846
Richard Harman <richard@richardharman.com>
2011-11-16 21:40:29 -0500

    Rework trie module to be nearly fully featured in comparison to Tree::Trie

--- lib/Net/DNS/Sinkhole/Trie.pm
+++ lib/Net/DNS/Sinkhole/Trie.pm
@@ -1,5 +1,5 @@
 package Net::DNS::Sinkhole::Trie;
-use Carp qw(croak);
+use Carp qw(carp);
 use parent qw(Tree::Trie);
 # vim: foldmethod=marker filetype=perl sw=2 commentstring=\ #\ %s
 
@@ -46,9 +46,22 @@ Exactly the same as Tree::Trie ->new, but has been modified to change the deepse
 # enhance Tree::Trie's new to default to exact
 
 sub new {  # {{{
-  my ($class,@args) = @_;
-  my $self = Tree::Trie->new({deepsearch=> "exact", @args});
+  my ($class,%args) = @_;
+
+  # we're not replacing Tree::Trie's new, just .. enhancing it.
+  # so our constructor is Tree::Trie's constructor.
+  my $self = $class->SUPER::new(
+    { %args,
+      freeze_end_marker => 1,
+      end_marker=>"",
+      deepsearch=> "exact",
+    }
+  );
   bless $self, $class;
+
+  # enable wildcard option
+  $self->{_WILDCARD} = 1 if $args{wildcard};
+
   return $self;
 } # }}}
 
@@ -95,70 +108,136 @@ Modified to quash case to lower case, and reverse the input and output Trie keys
 sub add { # {{{
   my ($self,@args) = @_;
 
-  # Tree::Trie supports adding an arrayref, but we don't.
-  map { croak("Adding references is unsupported") if ref($_) } @args;
+  # we expect strings to be fed to us, not arrayrefs.
+  carp("word reference encountered and ignored when expecting scalar string in add()") for grep { ref($_) } @args;
 
-  # add *.zone.com when adding zone.com automatically
-  my @wildcard = map { "*.$_" } @args;
-  @args = (@wildcard,@args);
+  # remove any references
+  @args = grep { ! ref($_) } @args;
 
-  my (@ret) = $self->SUPER::add(map { lc scalar reverse $_  } @args);
-  @ret = map { scalar reverse lc($_) } @ret;
-  wantarray ? @ret : scalar @ret;
+  # add wildcard, if enabled
+  @args = map { ("*.$_",$_) } @args if ($self->{_WILDCARD});
+
+  # quash lowercase, split on periods, reverse order, and make into an array reference.
+  @args = map { [ reverse split( m/\./, lc($_) ) ] } @args;
+
+  # We don't want to return arrayrefs, so convert them back into regular domain names.
+  my @ret = map { join(".",reverse @$_) } $self->SUPER::add(@args);
+
+  return wantarray ? @ret : scalar @ret;
+} # }}}
+
+sub add_all { # {{{
+  my ($self,@trees) = @_;
+  carp(sprintf("attempt to merge a possibly non-conforming format %s trie into a %s trie unsupported and ignored",ref($_),ref($self))) for grep { !$_->isa($self) } @trees;
+  $self->SUPER::add_all(grep { $_->isa($self) } @trees );
 } # }}}
 
 sub add_data { # {{{
   my ($self,@args) = @_;
-  my @wildcard;
 
-  for (my $i = 0; $i < $#args; $i+=2) {
-    $args[$i] = scalar reverse lc($args[$i]);
-    # add *.zone.com when adding zone.com automatically
-    push @wildcard,"$args[$i].*",$args[$i+1];
+  if ($self->{_WILDCARD}) {
+    my @wildcard;
+    for (my $i = 0; $i < $#args; $i += 2) {
+      push @wildcard, "*.".$args[$i], $args[$i+1];
+    }
+    push @args,@wildcard;
   }
 
-  my (@ret) = $self->SUPER::add_data(@args,@wildcard);
+  # quash keys lowercase, split on periods, reverse order, and make into an array reference.
+  for (my $i = 0; $i < $#args; $i+=2) {
+    if (ref($args[$i])) {
+      carp("word reference encountered and ignored when expecting scalar string in add_data()");
+      # remove those elements
+      splice(@args,$i,2);
+      $i-=2; next;
+    }
+    $args[$i] = [reverse split( m/\./,lc($args[$i]))];
+  }
+
+  my (@ret) = map { join(".",reverse @$_) } $self->SUPER::add_data(@args);
+
   wantarray ? @ret : scalar @ret;
 } # }}}
 
-sub lookup { # {{{
+sub remove { # {{{
   my ($self,@args) = @_;
-  $args[0] = scalar reverse lc($args[0]);
-  my (@ret) = $self->SUPER::lookup(@args);
-  if (@ret) { $ret[0] = scalar reverse $ret[0]; }
+
+  # we expect strings to be fed to us, not arrayrefs.
+  carp("word reference encountered and ignored when expecting scalar string in remove()") for grep { ref($_) } @args;
+
+  # remove any references
+  @args = grep { ! ref($_) } @args;
+
+  # add wildcard, if enabled
+  @args = map { ("*.$_",$_) } @args if ($self->{_WILDCARD});;
+
+  # quash lowercase, split on periods, and reverse order
+  @args = map { [reverse split( m/\./,lc($_))] } @args;
+
+  # We don't want to return arrayrefs, so convert them back into regular domain names.
+  my @ret = map { join(".",reverse @$_) } $self->SUPER::remove(@args);
+
+  wantarray ? @ret : scalar @ret;
+} # }}}
+
+sub delete_data {#{{{
+  my ($self,@args) = @_;
+
+  # add wildcard, if enabled
+  @args = map { ("*.$_",$_) } @args if ($self->{_WILDCARD});;
+
+  # quash lowercase, split on periods, and reverse order
+  @args = map { [reverse split( m/\./,lc($_))] } @args;
+
+  my @ret = map { join(".",reverse @$_) } $self->SUPER::delete_data(@args);
+
+  wantarray ? @ret : scalar @ret;
+}#}}}
+
+sub lookup { # {{{
+  my ($self,$word) = @_;
+  $word = [reverse split( m/\./,lc($word))];
+
+  # return value is variable based on list/scalar context
+  if (wantarray) {
+    my @ret = map { join(".",reverse @$_) } $self->SUPER::lookup($word);
+    return @ret;
+  } else {
+    my $ret = $self->SUPER::lookup($word);
+    return ref $ret ? join(".",reverse @$ret) : undef;
+  }
 } # }}}
 
 sub lookup_data { # {{{
-  my ($self,@args) = @_;
-  $args[0] = scalar reverse lc($args[0]);
-  $self->SUPER::lookup_data(@args);
+  my ($self,$word) = @_;
+  $word = [reverse split( m/\./,lc($word))];
+  $self->SUPER::lookup_data($word);
 } # }}}
 
-### Subroutines I havn't wrapped yet
+# No permission to change deepsearch.
+sub deepsearch { # {{{
+  my ($self) = @_;
+  return $self->SUPER::deepsearch(4);
+} # }}}
 
-=head1 DISABLED METHODS
+# No permission to change end_marker
+sub end_marker { # {{{
+  my ($self) = @_;
+  $self->SUPER::end_marker("");
+} # }}}
 
-This method has been intentionally disabled, because they have not been implemented yet.
+# no changes to freeze_end_marker
+sub freeze_end_marker { # {{{
+  my ($self) = @_;
+  $self->SUPER::freeze_end_marker(1);
+} # }}}
 
-=over 4
-
-=item remove
-
-=item delete_data
-
-=item deepsearch
-
-=item end_marker
-
-=back
-
-=cut
-
-sub add_all { my ($self) = @_; croak "add_all is unsupported"; }
-sub remove { my ($self) = @_; croak "remove is unsupported"; }
-sub delete_data { my ($self) = @_; croak "delete_data is unsupported"; }
-sub deepsearch { my ($self) = @_; croak "deepsearch is unsupported"; }
-sub end_marker { my ($self) = @_; croak "end_marker is unsupported"; }
-
+sub wildcard { # {{{
+  my ($self,$option) = @_;
+  if ($option) {
+    $self->{_WILCARD} = $option;
+  }
+  return $self->{_WILDCARD};
+} # }}}
 
 1;
